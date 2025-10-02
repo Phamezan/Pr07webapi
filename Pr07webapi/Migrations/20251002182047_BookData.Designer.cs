0015a01e714502856cfaf329e32e9704e96ee898
Phamezan <pham2003@outlook.dk>
2025-10-02 20:21:42 +0200

    migration med bookdata

--- Pr07webapi/Migrations/20251002182047_BookData.Designer.cs
+++ Pr07webapi/Migrations/20251002182047_BookData.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pr07webapi.Data;
 
@@ -10,9 +11,11 @@ using Pr07webapi.Data;
 namespace Pr07webapi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251002182047_BookData")]
+    partial class BookData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Pr07webapi/Migrations/20251002182047_BookData.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Pr07webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Title", "Year" },
+                values: new object[,]
+                {
+                    { 1, "Michael Bay", "Transformers", 2007 },
+                    { 2, "F. Scott Fitzgerald", "The Great Gatsby", 1925 },
+                    { 3, "Harper Lee", "To Kill a Mockingbird", 1960 },
+                    { 4, "George Orwell", "1984", 1949 },
+                    { 5, "J.K. Rowling", "Harry Potter and the Sorcerer's Stone", 1997 },
+                    { 6, "J.R.R. Tolkien", "The Hobbit", 1937 },
+                    { 7, "Jane Austen", "Pride and Prejudice", 1813 },
+                    { 8, "J.D. Salinger", "The Catcher in the Rye", 1951 },
+                    { 9, "J.R.R. Tolkien", "The Lord of the Rings", 1954 },
+                    { 10, "George Orwell", "Animal Farm", 1945 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
